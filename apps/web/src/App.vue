d12fd8dd40df9a1cf8557c90f8f6cb6b0542696a
Massimo Meert <pro@masmeert.com>
2023-08-25 12:05:06 +0200

    Add Basic MonoRepo

---
+++ apps/web/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { Button } from "ui";
+</script>
+
+<template>
+  <Button />
+</template>
---
+++ packages/ui/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="handleClick">Click Me</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+async function handleClick() {
+  alert("Hello there");
+}
+
+export default defineComponent({
+  name: "Button",
+  setup() {
+    return {
+      handleClick,
+    };
+  },
+});
+</script>
